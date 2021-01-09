ad7bf1c6f5d6eb78042282bbff8b3bbfd53b0f48
Chris Golden <golden.327@osu.edu>
2021-01-09 18:16:38 -0500

    init

---
+++ nconfig.js
@@ -0,0 +1,30 @@
+/**
+ * Project config for your site.
+ * All variables declared in `meta` will be accessible in your Nunjucks
+ * templates. Variables changed in `config` affect the build process.
+ *
+ */
+module.exports = {
+    // The `meta` object is sent to every template.
+    meta: {
+        title: "Christopher Golden's Blog",
+        description: "This blog is statically awesome!",
+        author: "Christopher Golden",
+    },
+    // All authors must be defined here.
+    authors: {
+        "first-author": {
+            name: "First Author",
+            short_bio: "short bio catchphrase",
+            bio: "a whole HTML paragraph."
+        },
+        "second-author": {
+            name: "Second Author",
+            short_bio: "a short bio.",
+            bio: "a whole HTML paragraph."
+        }
+    },
+    config: {
+
+    },
+}
